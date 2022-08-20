d0fb0f9a36241af6d2f359b15339695d144d3e89
Lior Ashkenazi <lior.ashkenazi@mail.huji.ac.il>
2022-08-20 20:09:25 +0300

    implemented initial version of the client's frontend

--- Client/ClientFrontend/frontend.py
+++ Client/ClientFrontend/frontend.py
@@ -1,31 +1,100 @@
 import sys
 
-# TODO change the names of the classes for separation
+from getpass import getpass
+
+import pyperclip
+
 from Client.ClientBackend.backend import ClientBackend
 from Server.ServerBackend.backend import ServerBackend
 
 from Client.ClientFrontend.messages import *
 from Client.ClientFrontend.inputs import *
 
+
 class Frontend:
-    SIGN_UP_MODE = "sign-up"
-    SIGN_IN_MODE = "sign-in"
+    SITE_IDX = 0
+    USERNAME_IDX = 1
+    PASSWORD_IDX = 2
 
     def __init__(self):
-        self.client_backend : ClientBackend
-        self.server_backend : ServerBackend
+        self.client_backend = ClientBackend()
+        self.server_backend = ServerBackend()
 
     def run(self):
+        self._login_screen()
+        self._main_screen()
+
+    def _login_screen(self):
         print(WELCOME_MSG)
-        while True:
-            print(LOG_IN_OPTS_MSG)
+        print(LOGIN_OPTS_MSG)
+        opt = input()
+        while opt not in LOGIN_OPTS:
+            print(INVALID_INPUT_MSG)
+            print(LOGIN_OPTS_MSG)
             opt = input()
-            while opt not in LOG_IN_OPTS:
-                print(INVALID_INPUT_MSG)
-                print(LOG_IN_OPTS_MSG)
+        while True:
             try:
                 username = input(USERNAME_MSG)
                 password = input(PASSWORD_MSG)
-                mode = Frontend.SIGN_UP_MODE if opt in SIGN_UP_OPT else Frontend.SIGN_IN_MODE
+                if opt == SIGN_UP_OPT:
+                    self.client_backend.register_user(username, password)
+                elif opt == SIGN_IN_OPT:
+                    self.client_backend.verify_user(username, password)
+                break
             except Exception as e:
-                print(e)
+                print(ERROR_PREFIX + str(e))
+        sys.stdout.flush()
+
+    def _main_screen(self):
+        # expecting to get list (empty or more elements) with all the login sites that are
+        # compromised
+        # TODO need to implement a method which given a compromised password know to relate
+        #  it to the relevant login sites
+        compromised_login_sites = self._psi()
+        if compromised_login_sites:
+            print(LEAKAGE_MSG)
+            print('\n'.join(compromised_login_sites))
+            print()
+        while True:
+            print(MAIN_OPTS_MSG)
+            opt = input()
+            while opt not in MAIN_OPTS:
+                print(INVALID_INPUT_MSG)
+                print(MAIN_OPTS_MSG)
+                opt = input()
+            try:
+                if opt == ADD_PASSWORD:
+                    self._add_password()
+                elif opt == RETRIEVE_PASSWORD:
+                    self._retrieve_password()
+                elif opt == RETRIEVE_ALL:
+                    self._retrieve_all_login_sites()
+                elif opt == QUIT:
+                    print(GOODBYE_MSG)
+                    break
+            except Exception as e:
+                print(ERROR_PREFIX + str(e))
+            sys.stdout.flush()
+
+    def _psi(self):
+        pass
+
+    def _add_password(self):
+        login_site = input(SITE_MSG)
+        login_username = input(USERNAME_MSG)
+        login_password = getpass(prompt=PASSWORD_MSG, stream="*")
+        self.client_backend.write_login_details(login_site, login_username, login_password)
+
+    def _retrieve_password(self):
+        login_site = input(SITE_MSG)
+        login_username, login_password = \
+            self.client_backend.retrieve_login_details(login_site)
+        print(RETRIEVED_INFO_MSG)
+        print(USERNAME_MSG + login_username)
+        pyperclip.copy(login_password)
+        print(PASSWORD_COPY_MSG)
+
+    def _retrieve_all_login_sites(self):
+        login_sites = self.client_backend.retrieve_all_login_details()
+        print(RETRIEVED_INFO_MSG)
+        print('\n'.join(login_sites))
--- Client/ClientFrontend/messages.py
+++ Client/ClientFrontend/messages.py
@@ -1,10 +1,34 @@
 TITLE = "Password Manager with PSI"
+
 WELCOME_MSG = "Welcome to Password Manager with PSI.\n"
-LOG_IN_OPTS_MSG = "Please select an option:\n" \
-              "\t\t* sign-up\n" \
-              "\t\t* sign-in\n"
+
+SELECT_OPTS_MSG = "Please select an option:\n"
+
+LOGIN_OPTS_MSG = SELECT_OPTS_MSG + "\t\t1. Sign-p\n" \
+                                   "\t\t2. Sign-in\n"
+
+MAIN_OPTS_MSG = SELECT_OPTS_MSG + "\t\t1. Add login details\n" \
+                                  "\t\t2. Retrieve login details\n" \
+                                  "\t\t3. Retrieve all login sites\n" \
+                                  "\t\t4. Quit\n"
+
+
+
+
+SITE_MSG = "Site: "
+USERNAME_MSG = "Username: "
+PASSWORD_MSG = "Password: "
+
+
+LEAKAGE_MSG = "Your passwords for the following login sites are leaked!"
+
+RETRIEVED_INFO_MSG = "Retrieved information:"
+
+PASSWORD_COPY_MSG = "Password copied to clipboard."
+
+GOODBYE_MSG = "Goodbye!"
 
 INVALID_INPUT_MSG = "Invalid input"
 
-USERNAME_MSG = "Username: "
-PASSWORD_MSG = "Password: "
+ERROR_PREFIX = "Error: "
+
