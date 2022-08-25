76723609c6bdc88395f6c7b0451cd71f9e5e7e54
Lior Ashkenazi <lior.ashkenazi@mail.huji.ac.il>
2022-08-25 15:07:08 +0300

    added unfinished version of update method for the server's backend

--- Server/ServerBackend/server_backend.py
+++ Server/ServerBackend/server_backend.py
@@ -1,20 +1,19 @@
-from Server.ServerBackend.passwordstable import PasswordsTable
-from Communication.tools import SocketFacade
 from Communication.params import SERVER_IP, SERVER_PORT
+from Communication.tools import SocketFacade
 from Crypto.PSI.server_side_psi import *
+from Server.ServerBackend.passwordstable import PasswordsTable
+
 
 class ServerBackend:
     def __init__(self):
+        self.updated = False
         self.passwords_table = PasswordsTable()
-        self.passwords = ['123456', 'asdasd', 'blabla']     # self.passwords_table.load()
+        self.passwords = self.passwords_table.load()
         print("S1")
         self.hash_table = insert_to_hash_table(self.passwords)
         print("S2")
         self.poly_coefficients = get_poly_coefficients(self.hash_table)
         print("S3")
-
-    def get_passwords(self):
-        return self.passwords_table.load()
 
     def run(self):
         """
@@ -30,3 +29,9 @@ class ServerBackend:
                 client_powers_vector = calculate_encrypted_powers(encrypted_client_query)
                 response = get_server_response(client_powers_vector, self.poly_coefficients)
                 s.send_msg(response)
+
+    def update(self, new_passwords: List[str]):
+        # TODO implement multi-threading for updating self.hash_table and self.poly_coefficients
+        self.passwords += new_passwords
+        self.passwords_table.save(self.passwords)
+        self.updated = True
